04d92104532a868c07f898f82ed241ec80405b25
outcastgeek <outcastgeek+git@gmail.com>
2014-08-06 00:07:54 -0500

    Added PostgreSQL and Updated Cmd Script

--- cmd.sh
+++ cmd.sh
@@ -25,8 +25,19 @@ case $1 in
   build_es)
     docker build -t outcastgeek/es $2
     ;;
-  run_es)
-    docker run -d -p $2 outcastgeek/es
+  run_es) # docker run -d -p 9200:9200 -p 9300:9300 dockerfile/elasticsearch
+          # docker run -d -p 9200:9200 -p 9300:9300 -v <data-dir>:/data dockerfile/elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+    docker run -d -p $2:9200 -p $3:9300 outcastgeek/es
+    #docker run -d -p $2 -p $3 -v $4:/data outcastgeek/es /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+    ;;
+  build_pg)
+    docker build -t outcastgeek/pg $2
+    ;;
+  run_pg) # run -d --name postgresql -p 5432:5432 -e POSTGRESQL_PASS=oe9jaacZLbR9pN imanel/postgresql
+    docker run -d --name postgresql -p $2:5432 -e POSTGRESQL_PASS=$3 outcastgeek/pg
+    ;;
+  pg_client) # docker run -i --rm -t --entrypoint="bash" --link postgresql:postgresql imanel/postgresql -c 'psql -h $POSTGRESQL_PORT_5432_TCP_ADDR --user postgres'
+    docker run -i --rm -t --entrypoint="bash" --link postgresql:postgresql outcastgeek/pg -c 'psql -h $POSTGRESQL_PORT_5432_TCP_ADDR --user postgres'
     ;;
   esac
 exit 0
